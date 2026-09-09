46cd9a58c09a2aff90f2511dc3d30421264aa31a
antho <anthony.pruniere@wanadoo.fr>
2026-09-09 16:19:00 +0200

    ajout exercice conditions

--- src/app/pages/exercices/signals-exercice/signals-exercice.ts
+++ src/app/pages/exercices/signals-exercice/signals-exercice.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  imports: [FormsModule],
+  imports: [CommonModule],
   selector: 'app-signals-exercice',
   styleUrl: './signals-exercice.css',
   templateUrl: './signals-exercice.html',
 })
-export class SignalsExercice {}
+export class SignalsExercice {
+  count = signal(0);
+
+  increment() {
+    this.count.set(this.count() + 1);
+  }
+
+  decrement() {
+    this.count.update(value => Math.max(0, value - 1));    
+  }
+
+  reset() {
+    this.count.set(0);
+  }
+}
